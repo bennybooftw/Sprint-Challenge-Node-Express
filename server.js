7a6306b388d8a3da559c9911e2de5db04cb2e0ed
Ben <bennybooftw@gmail.com>
2018-12-08 13:03:22 -0500

    Created GET for action id

--- server.js
+++ server.js
@@ -24,4 +24,25 @@ server.get('/api/actions', (req, res) => {
     );
 });
 
+server.get('/api/actions/:id', (req, res) => {
+  const { id } = req.params;
+  action
+    .get(id)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({
+          message: 'The action with the specified ID does not exist.'
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The action information could not be retrieved.',
+        error: err
+      });
+    }); 
+});
+
 module.exports = server;
